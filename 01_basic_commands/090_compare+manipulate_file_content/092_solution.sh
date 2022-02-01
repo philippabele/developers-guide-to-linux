c7915884835b3b9c3f7baf9c0c8398be976549e0
Liam Friedrich <liam.friedrich@outlook.com>
2022-02-01 10:54:06 +0100

    vim manual adde

---
+++ 01_basic_commands/090_compare+manipulate_file_content/092_solution.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+vim -d testfile2 testfile1
+# Scroll up using the arrow keys till you find the highlighted difference
+# select the text in the second window using the cursor and copy it by right clicking
+# press i to get into edit mode
+# remove the difference using Backspace or Del
+# insert the copied text using right click
+# Press ESC - :wq - :q - To exit the editors
